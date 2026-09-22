0bcd5ce59e9213f6deff52baa811b6a0399bf7bb
Koimi Yovo <yovoedem33@gmail.com>
2026-09-23 01:40:58 +0200

    [transaction] get transaction

---
+++ domain/src/main/kotlin/com/kyovo/cents/domain/port/input/GetTransactionUseCase.kt
@@ -0,0 +1,9 @@
+package com.kyovo.cents.domain.port.input
+
+import com.kyovo.cents.domain.model.Transaction
+import com.kyovo.cents.domain.model.TransactionId
+
+interface GetTransactionUseCase
+{
+    fun get(id: TransactionId): Transaction?
+}
