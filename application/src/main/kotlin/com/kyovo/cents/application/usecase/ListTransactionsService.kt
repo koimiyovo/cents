c1996862e628d5df0a252679edff03f1c36c5022
Koimi Yovo <yovoedem33@gmail.com>
2026-09-23 14:19:49 +0200

    [transaction] filter transaction by subcategorie rather than categorie

--- application/src/main/kotlin/com/kyovo/cents/application/usecase/ListTransactionsService.kt
+++ application/src/main/kotlin/com/kyovo/cents/application/usecase/ListTransactionsService.kt
@@ -2,7 +2,7 @@ package com.kyovo.cents.application.usecase
 
 import com.kyovo.cents.domain.model.AccountId
 import com.kyovo.cents.domain.model.Transaction
-import com.kyovo.cents.domain.model.TransactionCategory
+import com.kyovo.cents.domain.model.TransactionSubcategory
 import com.kyovo.cents.domain.port.input.ListTransactionsUseCase
 import com.kyovo.cents.domain.port.output.TransactionRepository
 import java.time.Instant
@@ -12,7 +12,7 @@ class ListTransactionsService(private val transactionRepository: TransactionRepo
 {
     override fun list(
         accountId: AccountId?,
-        category: TransactionCategory?,
+        subcategory: TransactionSubcategory?,
         from: Instant?,
         to: Instant?,
         titleFilter: String
@@ -21,7 +21,7 @@ class ListTransactionsService(private val transactionRepository: TransactionRepo
         return transactionRepository.findAll()
             .filter { transaction ->
                 (accountId == null || transaction.accountId == accountId) &&
-                        (category == null || transaction.category == category) &&
+                        (subcategory == null || transaction.subcategory == subcategory) &&
                         (from == null || !transaction.date.isBefore(from)) &&
                         (to == null || !transaction.date.isAfter(to)) &&
                         transaction.title.contains(titleFilter)
